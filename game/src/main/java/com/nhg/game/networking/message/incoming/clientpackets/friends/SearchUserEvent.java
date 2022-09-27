0606871a48087c2e1369069411232eae79f1e89c
Izzxt <haziqizzat89@gmail.com>
2022-09-27 23:35:09 +0800

    Implement search user event

---
+++ game/src/main/java/com/nhg/game/networking/message/incoming/clientpackets/friends/SearchUserEvent.java
@@ -0,0 +1,44 @@
+package com.nhg.game.networking.message.incoming.clientpackets.friends;
+
+import com.nhg.game.dto.UserSearchFilterRequest;
+import com.nhg.game.dto.UserSearchFilterResponse;
+import com.nhg.game.networking.WebSocketClient;
+import com.nhg.game.networking.message.incoming.ClientPacket;
+import com.nhg.game.networking.message.outgoing.OutgoingPacketHeaders;
+import com.nhg.game.networking.message.outgoing.ServerPacket;
+import com.nhg.game.user.User;
+import com.nhg.game.user.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Slf4j
+public class SearchUserEvent extends ClientPacket {
+
+    private final RestTemplate restTemplate;
+    private final UserService userService;
+
+    public SearchUserEvent() {
+        restTemplate = this.getBean("restTemplate", RestTemplate.class);
+        userService = this.getBean(UserService.class);
+    }
+
+    @Override
+    public void handle() {
+        try {
+            WebSocketClient wsClient = (WebSocketClient) client;
+
+            String username = body.getString("username");
+
+            List<User> userList = userService.filterUser(username);
+
+            wsClient.sendMessage(new ServerPacket(
+                    OutgoingPacketHeaders.UserSearchFiltering,
+                    userList
+            ));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+}
