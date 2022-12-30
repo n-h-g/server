7e4ca57d4826d2eb2eb37b1856a5880eded1fb2b
Mattia Shux <shuxdev@gmail.com>
2022-12-31 00:04:23 +0100

    Friendship model

--- messenger/src/main/java/com/nhg/messenger/model/Friendship.java
+++ messenger/src/main/java/com/nhg/messenger/model/Friendship.java
@@ -3,6 +3,8 @@ package com.nhg.messenger.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -12,22 +14,46 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Friendship {
 
-    @Id
-    @GeneratedValue(
-            generator = "sequence_friendship_id",
-            strategy = GenerationType.SEQUENCE
-    )
-    @SequenceGenerator(
-            name = "sequence_friendship_id",
-            sequenceName = "sequence_friendship_id"
-    )
-    private Integer id;
+    @Getter
+    @Setter
+    @Embeddable
+    public static class Id implements Serializable {
+        /**
+         * One of the two users who forms the friendship.
+         * In case <code>pending</code> is true, it represents the sender of a friendship request.
+         *
+         * @see #pending
+         */
+        private Integer user1;
+        /**
+         * One of the two users who forms the friendship.
+         * In case <code>pending</code> is true, it represents the recipient of a friendship request.
+         *
+         * @see #pending
+         */
+        private Integer user2;
 
-    @Column(nullable = false)
-    private Integer senderId;
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Id that = (Id) o;
+            return user1.equals(that.user1) && user2.equals(that.user2)
+                    || user1.equals(that.user2) && user2.equals(that.user1);
+        }
 
-    @Column(nullable = false)
-    private Integer destinationId;
+        @Override
+        public int hashCode() {
+            return Objects.hash(user1, user2);
+        }
+    }
 
+    @EmbeddedId
+    private Id id;
+
+    /**
+     * If it's true, this class represents a friendship request,
+     * otherwise it represents an already accepted friendship.
+     */
     private boolean pending;
 }
