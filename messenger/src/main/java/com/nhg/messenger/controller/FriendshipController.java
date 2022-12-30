b6283cbbe3397c1cc5a14e695c7adc9c71d87718
Mattia Shux <shuxdev@gmail.com>
2022-12-30 23:37:59 +0100

    Removed FriendshipController methods

--- messenger/src/main/java/com/nhg/messenger/controller/FriendshipController.java
+++ messenger/src/main/java/com/nhg/messenger/controller/FriendshipController.java
@@ -16,23 +16,4 @@ public class FriendshipController {
 
     private final FriendshipService friendshipService;
 
-    @GetMapping("/get_friends/{senderId}")
-    public FriendshipResponse getFriends(FriendshipRequest friendshipRequest) {
-        return friendshipService.getFriendsById(friendshipRequest.senderId());
-    }
-
-    @GetMapping("/accept/{senderId}/{destinationId}")
-    public FriendshipResponse acceptFriendShip(FriendshipRequest friendshipRequest) {
-        return friendshipService.acceptFriendship(friendshipRequest);
-    }
-
-    @GetMapping("/add/{senderId}/{destinationId}")
-    public FriendResponse addFriend(FriendshipRequest friendshipRequest) {
-        return friendshipService.addFriend(friendshipRequest);
-    }
-
-    @GetMapping("/delete/{senderId}/{destinationId}")
-    public FriendshipResponse removeFriendship(FriendshipRequest friendshipRequest) {
-        return friendshipService.removeFriendship(friendshipRequest);
-    }
 }
