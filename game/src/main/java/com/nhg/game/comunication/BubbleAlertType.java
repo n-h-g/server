953b8876ff0d0b8843dae4ed60d11a175b6b3f56
Mattia Shux <shuxdev@gmail.com>
2022-12-31 10:42:04 +0100

    refactoring

--- game/src/main/java/com/nhg/game/comunication/BubbleAlertType.java
+++ game/src/main/java/com/nhg/game/comunication/BubbleAlertType.java
@@ -1,11 +1,11 @@
-package com.nhg.game.utils;
+package com.nhg.game.comunication;
 
 import lombok.Getter;
 
 public enum BubbleAlertType {
-    Default("DefaultBubbleAction"),
-    Friend("FriendBubbleAction"),
-    Purchase("PurchaseInfoAction");
+    Default("default_bubble_action"),
+    Friend("friend_bubble_action"),
+    Purchase("purchase_bubble_action");
 
     @Getter
     private final String value;
