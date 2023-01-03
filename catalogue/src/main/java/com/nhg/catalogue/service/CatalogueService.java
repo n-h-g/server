ce2799760ec67d8f20334347c98a2d0eeb754f17
Mattia Shux <shuxdev@gmail.com>
2023-01-04 00:27:42 +0100

    CatalogueService

---
+++ catalogue/src/main/java/com/nhg/catalogue/service/CatalogueService.java
@@ -0,0 +1,35 @@
+package com.nhg.catalogue.service;
+
+import com.nhg.catalogue.model.Item;
+import com.nhg.catalogue.model.Page;
+import com.nhg.catalogue.repository.ItemRepository;
+import com.nhg.catalogue.repository.PageRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+@Service
+@AllArgsConstructor
+public class CatalogueService {
+
+    private final PageRepository pageRepository;
+    private final ItemRepository itemRepository;
+
+    public Set<Page> getAllPages() {
+        Iterator<Page> iterator = pageRepository.findAll().iterator();
+
+        SortedSet<Page> pages = new TreeSet<>();
+        iterator.forEachRemaining(pages::add);
+
+        return pages;
+    }
+
+    public List<Item> getItemsForPage(int pageId) {
+        return itemRepository.findByPageId(pageId);
+    }
+}
