f151b7a1aa322699a9b44134345468a9feb918c3
Mattia Shux <shuxdev@gmail.com>
2023-01-04 13:11:58 +0100

    Removed allowStack and stackHeight

--- game/src/main/java/com/nhg/game/utils/pathfinder/Tile.java
+++ game/src/main/java/com/nhg/game/utils/pathfinder/Tile.java
@@ -1,13 +1,12 @@
 package com.nhg.game.utils.pathfinder;
 
-
 import com.nhg.game.utils.Int3;
 import lombok.Getter;
 import lombok.Setter;
 
 public class Tile implements Comparable<Tile> {
 
-    public enum State { OPEN, CLOSE, INVALID };
+    public enum State { OPEN, CLOSE }
 
     @Getter
     @Setter
@@ -28,20 +27,9 @@ public class Tile implements Comparable<Tile> {
     @Setter
     private Tile previousTile;
 
-
-    @Getter
-    @Setter
-    private boolean allowStack;
-
-    @Getter
-    @Setter
-    private double stackHeight;
-
     public Tile(Int3 position) {
         this.position = position;
         this.state = position.getZ() == 0 ? State.CLOSE : State.OPEN;
-        this.allowStack = false;
-        this.stackHeight = 0;
     }
 
     public double getFCost() {
@@ -52,26 +40,5 @@ public class Tile implements Comparable<Tile> {
     public int compareTo(Tile o) {
         double dif = getFCost() - o.getFCost();
         return dif == 0 ? 0 : dif > 0 ? 1 : -1;
-    }
-
-    public boolean isWalkable() {
-        return this.state == State.OPEN;
-    }
-
-    public void setStackHeight(double stackHeight) {
-        if(this.state == State.INVALID) {
-            this.stackHeight = Double.MAX_VALUE;
-            this.allowStack = false;
-            return;
-        }
-
-        if (stackHeight >= 0 && stackHeight != Double.MAX_VALUE) {
-            this.stackHeight = stackHeight;
-            this.allowStack = true;
-        } else {
-            this.allowStack = false;
-            this.stackHeight = this.position.getZ();
-        }
-
     }
 }
