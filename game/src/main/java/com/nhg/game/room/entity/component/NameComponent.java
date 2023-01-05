0848354da3f0d3a8273a33e67d2d56ed4b65d673
Mattia Shux <shuxdev@gmail.com>
2023-01-05 19:07:54 +0100

    NameComponent constructor

--- game/src/main/java/com/nhg/game/room/entity/component/NameComponent.java
+++ game/src/main/java/com/nhg/game/room/entity/component/NameComponent.java
@@ -1,6 +1,7 @@
 package com.nhg.game.room.entity.component;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,6 +11,10 @@ public class NameComponent extends Component {
     @Getter
     @Setter
     private String name;
+
+    public NameComponent(@NonNull String name) {
+        this.name = name;
+    }
 
     @Override
     public JSONObject toJson() throws JSONException {
