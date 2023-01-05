1ff87f2cee519d90abf6d05c34d7a44fcc760644
Mattia Shux <shuxdev@gmail.com>
2023-01-05 18:29:52 +0100

    ComponentType

---
+++ game/src/main/java/com/nhg/game/room/entity/component/ComponentType.java
@@ -0,0 +1,26 @@
+package com.nhg.game.room.entity.component;
+
+import lombok.Getter;
+
+public enum ComponentType {
+    User("user", UserComponent.class),
+    Position("position", PositionComponent.class),
+    Movement("movement", MovementComponent.class),
+    BodyHeadRotation("bh_rot", BodyHeadRotationComponent.class);
+
+    @Getter
+    private final String name;
+
+    private final Class<? extends Component> clazz;
+
+    ComponentType(String name, Class<? extends Component> clazz) {
+        this.name = name;
+        this.clazz = clazz;
+    }
+
+    public Class<? extends Component> getComponentClass() {
+        return clazz;
+    }
+
+
+}
