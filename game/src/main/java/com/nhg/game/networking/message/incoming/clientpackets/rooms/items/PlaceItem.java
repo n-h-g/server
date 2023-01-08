f716818c810e25318e38c5c4a98b383dbb58b5e2
Mattia Shux <shuxdev@gmail.com>
2023-01-08 16:38:09 +0100

    PlaceItem packet

---
+++ game/src/main/java/com/nhg/game/networking/message/incoming/clientpackets/rooms/items/PlaceItem.java
@@ -0,0 +1,54 @@
+package com.nhg.game.networking.message.incoming.clientpackets.rooms.items;
+
+import com.nhg.game.item.Item;
+import com.nhg.game.item.ItemService;
+import com.nhg.game.networking.WebSocketClient;
+import com.nhg.game.networking.message.incoming.ClientPacket;
+import com.nhg.game.networking.message.outgoing.OutgoingPacketHeaders;
+import com.nhg.game.networking.message.outgoing.ServerPacket;
+import com.nhg.game.room.Room;
+import com.nhg.game.room.RoomService;
+import com.nhg.game.user.User;
+import com.nhg.game.utils.BeanRetriever;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PlaceItem extends ClientPacket {
+
+    private RoomService roomService;
+    private ItemService itemService;
+
+    public PlaceItem() {
+        roomService = BeanRetriever.get(RoomService.class);
+        itemService = BeanRetriever.get(ItemService.class);
+    }
+
+    @Override
+    public void handle() {
+        try {
+            WebSocketClient wsClient = (WebSocketClient) client;
+            User user = wsClient.getUser();
+
+            if (user == null) return;
+
+            Room room = user.getEntity().getRoom();
+
+            if (room == null) return;
+
+            int itemId = body.getInt("id");
+
+            Item item = itemService.getItemByIdAndOwner(user, itemId);
+
+            if (item == null) return;
+
+            if (!roomService.placeItem(user, item, room)) return;
+
+            room.getUsers().sendBroadcastMessage(
+                    new ServerPacket(OutgoingPacketHeaders.AddRoomEntity, item.getEntity()));
+
+        } catch (Exception e) {
+            log.error("Error: "+ e);
+            e.printStackTrace();
+        }
+    }
+}
