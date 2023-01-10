59bd499082e0be6ffc34cba1803a1f1b535ebee1
Mattia Shux <shuxdev@gmail.com>
2023-01-10 11:53:43 +0100

    CataloguePages

---
+++ game/src/main/java/com/nhg/game/networking/message/incoming/clientpackets/catalogue/CataloguePages.java
@@ -0,0 +1,44 @@
+package com.nhg.game.networking.message.incoming.clientpackets.catalogue;
+
+import com.nhg.game.dto.CataloguePageRequest;
+import com.nhg.game.networking.WebSocketClient;
+import com.nhg.game.networking.message.incoming.ClientPacket;
+import com.nhg.game.networking.message.outgoing.OutgoingPacketHeaders;
+import com.nhg.game.networking.message.outgoing.ServerPacket;
+import com.nhg.game.user.User;
+import com.nhg.game.utils.BeanRetriever;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+public class CataloguePages extends ClientPacket {
+
+    private final RestTemplate restTemplate;
+
+    public CataloguePages() {
+        restTemplate = BeanRetriever.get("restTemplate", RestTemplate.class);
+    }
+
+    @Override
+    public void handle() {
+        try {
+            WebSocketClient wsClient = (WebSocketClient) client;
+            User user = wsClient.getUser();
+
+            if (user == null) return;
+
+            CataloguePageRequest[] pages = restTemplate.getForEntity(
+                    "http://CATALOGUE/api/v1/catalogue/page",
+                    CataloguePageRequest[].class
+            ).getBody();
+
+            if (pages == null) return;
+
+            client.sendMessage(new ServerPacket(OutgoingPacketHeaders.CataloguePages, pages));
+
+        } catch(Exception e) {
+            log.error("Error: "+ e);
+            e.printStackTrace();
+        }
+    }
+}
