f8a03a0d1ce2d31ba63a54a219fb0cd4610e3f7d
Mattia Shux <shuxdev@gmail.com>
2023-01-11 09:29:37 +0100

    BotSpeech

---
+++ game/src/main/java/com/nhg/game/bot/BotSpeech.java
@@ -0,0 +1,34 @@
+package com.nhg.game.bot;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class BotSpeech {
+
+    @Id
+    @GeneratedValue(
+            generator = "sequence_bot_speech_id",
+            strategy = GenerationType.SEQUENCE
+    )
+    @SequenceGenerator(
+            name = "sequence_bot_speech_id",
+            sequenceName = "sequence_bot_speech_id"
+    )
+    private Integer id;
+
+    @Column(nullable = false)
+    private String speech;
+
+    @ManyToOne
+    @JoinColumn(columnDefinition="integer", name="bot_id")
+    private Bot bot;
+
+
+}
