48a54959e4f98649e7f7e347e63f7973dcace590
Mattia Shux <shuxdev@gmail.com>
2023-01-11 10:11:27 +0100

    ItemSpecificationRepository @Repository annotation

--- game/src/main/java/com/nhg/game/item/ItemSpecificationRepository.java
+++ game/src/main/java/com/nhg/game/item/ItemSpecificationRepository.java
@@ -1,7 +1,9 @@
 package com.nhg.game.item;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemSpecificationRepository extends CrudRepository<ItemSpecification, Integer> {
     ItemSpecification findByName(String name);
 }
