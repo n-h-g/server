5354f85e1f347de5087d8f398411774c7d1dc367
Mattia Shux <shuxdev@gmail.com>
2023-01-11 10:14:15 +0100

    BotRepository

---
+++ game/src/main/java/com/nhg/game/bot/BotRepository.java
@@ -0,0 +1,13 @@
+package com.nhg.game.bot;
+
+import com.nhg.game.room.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BotRepository extends CrudRepository<Bot, Integer> {
+
+    List<Bot> findByRoom(Room room);
+}
