5b1ee07666d0db2c35ba3f4877bfa740378a12a8
Mattia <82611659+Xhst@users.noreply.github.com>
2024-01-28 09:40:54 +0100

    Updated dependencies from Netflix eureka to consul

--- game/src/main/java/com/nhg/game/GameApplication.java
+++ game/src/main/java/com/nhg/game/GameApplication.java
@@ -2,12 +2,9 @@ package com.nhg.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.DependsOn;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GameApplication {
 
