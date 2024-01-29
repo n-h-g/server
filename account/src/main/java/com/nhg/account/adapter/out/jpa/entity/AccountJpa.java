6dd9138bf135d1293852436122c074a53e4b6fb9
Mattia <82611659+Xhst@users.noreply.github.com>
2024-01-29 10:32:03 +0100

    Account Jpa

---
+++ account/src/main/java/com/nhg/account/adapter/out/jpa/entity/AccountJpa.java
@@ -0,0 +1,52 @@
+package com.nhg.account.adapter.out.jpa.entity;
+
+import com.nhg.account.domain.Account;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountJpa {
+
+    @Id
+    @GeneratedValue(
+            generator = "sequence_account_id",
+            strategy = GenerationType.SEQUENCE
+    )
+    @SequenceGenerator(
+            name = "sequence_account_id",
+            sequenceName = "sequence_account_id"
+    )
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Account toAccount() {
+        return Account.builder()
+                .id(getId())
+                .username(getUsername())
+                .email(getEmail())
+                .password(getPassword())
+                .build();
+    }
+
+}
