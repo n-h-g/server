42b95da1af7b53ab31e82cfeadabbfbdaa17f808
Mattia <82611659+Xhst@users.noreply.github.com>
2024-01-30 15:49:21 +0100

    UpdateUser packet

---
+++ game/src/main/java/com/nhg/game/adapter/in/websocket/user/UpdateUser.java
@@ -0,0 +1,34 @@
+package com.nhg.game.adapter.in.websocket.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhg.game.adapter.in.websocket.IncomingPacket;
+import com.nhg.game.adapter.out.websocket.OutPacketHeaders;
+import com.nhg.game.application.dto.UserDto;
+import com.nhg.game.domain.user.User;
+import com.nhg.game.infrastructure.context.BeanRetriever;
+import com.nhg.game.infrastructure.networking.ClientUserMap;
+import com.nhg.game.infrastructure.networking.OutgoingPacket;
+
+public class UpdateUser extends IncomingPacket {
+
+    private final ClientUserMap<String> clientUserMap;
+    private final ObjectMapper objectMapper;
+
+    @SuppressWarnings("unchecked")
+    public UpdateUser() {
+        clientUserMap = BeanRetriever.get(ClientUserMap.class);
+        objectMapper = BeanRetriever.get(ObjectMapper.class);
+    }
+
+    @Override
+    public void handle() throws Exception {
+        User user = clientUserMap.get((String) client.getId());
+
+        if (user == null) return;
+
+        client.sendMessage(new OutgoingPacket(
+                OutPacketHeaders.UpdateUserInformation,
+                objectMapper.writeValueAsString(UserDto.fromDomain(user))
+        ));
+    }
+}
