06fc70c05cdb58acb344df27104142d2feb02bf9
Mattia <82611659+Xhst@users.noreply.github.com>
2024-01-30 12:54:50 +0100

    Websocket config

--- game/src/main/java/com/nhg/game/infrastructure/networking/websocket/WebSocketConfig.java
+++ game/src/main/java/com/nhg/game/infrastructure/networking/websocket/WebSocketConfig.java
@@ -1,8 +1,9 @@
-package com.nhg.game.networking;
+package com.nhg.game.infrastructure.networking.websocket;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
