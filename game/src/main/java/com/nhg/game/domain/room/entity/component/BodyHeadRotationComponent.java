4d76d71b64b73246a3e003cc3db252eae0f819be
Mattia <82611659+Xhst@users.noreply.github.com>
2024-01-31 08:21:03 +0100

    BodyHeadRotationComponent

---
+++ game/src/main/java/com/nhg/game/domain/room/entity/component/BodyHeadRotationComponent.java
@@ -0,0 +1,38 @@
+package com.nhg.game.domain.room.entity.component;
+
+
+import com.nhg.game.domain.shared.position.Rotation;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BodyHeadRotationComponent extends Component {
+
+
+    private Rotation bodyRotation;
+    private Rotation headRotation;
+
+    public BodyHeadRotationComponent() {
+        setRotation(Rotation.SOUTH);
+    }
+
+    public BodyHeadRotationComponent(Rotation rotation) {
+        setRotation(rotation);
+    }
+
+    /**
+     * Sets body and head rotation to the given rotation.
+     *
+     * @param rotation the rotation to set.
+     */
+    public void setRotation(Rotation rotation) {
+        bodyRotation = rotation;
+        headRotation = rotation;
+    }
+
+    @Override
+    public void cycle() {
+
+    }
+}
