84bf3085ec82687d99fc82ba7ab97f39e321db00
Mattia <82611659+Xhst@users.noreply.github.com>
2024-01-31 11:35:11 +0100

    RoomRepository renamed get to find

--- game/src/main/java/com/nhg/game/application/repository/RoomRepository.java
+++ game/src/main/java/com/nhg/game/application/repository/RoomRepository.java
@@ -2,6 +2,13 @@ package com.nhg.game.application.repository;
 
 import com.nhg.game.domain.room.Room;
 
+import java.util.Optional;
+
 public interface RoomRepository {
+
     Room save(Room room);
+
+    Optional<Room> find(int roomId);
+
+    void delete(int id);
 }
