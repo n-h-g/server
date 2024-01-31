ab58c07062d610c2d1be96fa49f51dff6cbeea27
Mattia <82611659+Xhst@users.noreply.github.com>
2024-01-31 11:43:51 +0100

    Removed NoArgsConstructor

--- game/src/main/java/com/nhg/game/application/event/room/RoomCreatedEvent.java
+++ game/src/main/java/com/nhg/game/application/event/room/RoomCreatedEvent.java
@@ -3,11 +3,9 @@ package com.nhg.game.application.event.room;
 import com.nhg.common.domain.event.DomainEvent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class RoomCreatedEvent implements DomainEvent {
 
     private int roomId;
