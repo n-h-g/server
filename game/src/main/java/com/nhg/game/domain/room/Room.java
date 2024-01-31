e52212d7d330b613fc00436b84b38fc9ce488297
Mattia <82611659+Xhst@users.noreply.github.com>
2024-01-31 07:39:02 +0100

    Room users

--- game/src/main/java/com/nhg/game/domain/room/Room.java
+++ game/src/main/java/com/nhg/game/domain/room/Room.java
@@ -5,7 +5,11 @@ import com.nhg.game.domain.room.layout.RoomLayout;
 import com.nhg.game.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Getter
 @Setter
@@ -18,7 +22,10 @@ public class Room implements Runnable {
     private User owner;
     private RoomLayout roomLayout;
 
+    private final Map<Integer, User> users;
+
     public Room() {
+        this.users = new ConcurrentHashMap<>();
     }
 
     public Room(String name, String description, User owner, String layout, int doorX, int doorY, int doorRotation) {
@@ -30,8 +37,34 @@ public class Room implements Runnable {
         this.roomLayout = new RoomLayout(layout, doorX, doorY, doorRotation);
     }
 
+    /**
+     * The specified user enters the room and is added to the user group in the room.
+     * Then an entity is associated with the user, created and added to the entities' collection.
+     *
+     * @param user the user entering the room.
+     */
+    public void userEnter(@NonNull User user) {
+        users.put(user.getId(), user);
+    }
+
+    /**
+     * The specified user exits the room and is removed from the user group in the room.
+     * If the user has an entity in the room it's removed and all the other users are notified.
+     *
+     * @param user the user exiting the room.
+     */
+    public void userExit(@NonNull User user) {
+        users.remove(user.getId());
+    }
+
+    /**
+     * Check if the room is empty.
+     * The room is empty when it has 0 users inside.
+     *
+     * @return true if it's empty, false otherwise.
+     */
     public boolean isEmpty() {
-        return true;
+        return users.isEmpty();
     }
 
     @Override
