53d8adb5ebe47230f6681200e6b15c4753b3fcb5
Mattia <82611659+Xhst@users.noreply.github.com>
2024-01-31 10:05:36 +0100

    SpringEventPublisher

---
+++ game/src/main/java/com/nhg/game/adapter/out/event/SpringEventPublisher.java
@@ -0,0 +1,19 @@
+package com.nhg.game.adapter.out.event;
+
+import com.nhg.common.domain.event.DomainEvent;
+import com.nhg.game.application.event.GameEventPublisher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SpringEventPublisher implements GameEventPublisher {
+
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    @Override
+    public void publish(DomainEvent event) {
+        applicationEventPublisher.publishEvent(event);
+    }
+}
