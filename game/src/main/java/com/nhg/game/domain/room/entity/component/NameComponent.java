68c007db264fb3f42703e278795c2d5384608fe0
Mattia <82611659+Xhst@users.noreply.github.com>
2024-01-31 08:20:34 +0100

    NameComponent

---
+++ game/src/main/java/com/nhg/game/domain/room/entity/component/NameComponent.java
@@ -0,0 +1,21 @@
+package com.nhg.game.domain.room.entity.component;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NameComponent extends Component {
+
+    private String name;
+
+    public NameComponent(@NonNull String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void cycle() {
+
+    }
+}
