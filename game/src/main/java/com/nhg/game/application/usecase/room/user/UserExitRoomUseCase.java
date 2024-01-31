a730fe0bee9724ebbeac9ddd80440877a46045c6
Mattia <82611659+Xhst@users.noreply.github.com>
2024-01-31 12:01:59 +0100

    UserExitRoomUseCase

---
+++ game/src/main/java/com/nhg/game/application/usecase/room/user/UserExitRoomUseCase.java
@@ -0,0 +1,14 @@
+package com.nhg.game.application.usecase.room.user;
+
+import com.nhg.common.domain.UseCase;
+import com.nhg.game.domain.user.User;
+import lombok.NonNull;
+
+@UseCase
+public class UserExitRoomUseCase {
+
+
+    public void userExitRoom(@NonNull User user) {
+        // TODO
+    }
+}
