94d82b1d3864d98bb4889bff5db0cbab8af254f9
Mattia <82611659+Xhst@users.noreply.github.com>
2024-02-01 09:03:48 +0100

    User builder

--- game/src/main/java/com/nhg/game/domain/user/User.java
+++ game/src/main/java/com/nhg/game/domain/user/User.java
@@ -3,11 +3,13 @@ package com.nhg.game.domain.user;
 
 import com.nhg.game.domain.shared.human.Gender;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 public class User {
 
