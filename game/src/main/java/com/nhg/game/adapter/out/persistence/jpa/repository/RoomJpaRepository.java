8b80f61fbd7da3b65d07450e4c101f8644bf17da
RealCosis <realcosis@gmail.com>
2024-02-01 20:51:50 +0100

    Fixed RoomJpa entity and repository & UserJpa from domain
    Co-Authored-By: Mattia <82611659+Xhst@users.noreply.github.com>

--- game/src/main/java/com/nhg/game/adapter/out/persistence/jpa/repository/RoomJpaRepository.java
+++ game/src/main/java/com/nhg/game/adapter/out/persistence/jpa/repository/RoomJpaRepository.java
@@ -1,6 +1,7 @@
 package com.nhg.game.adapter.out.persistence.jpa.repository;
 
 import com.nhg.game.adapter.out.persistence.jpa.entity.RoomJpa;
+import com.nhg.game.adapter.out.persistence.jpa.entity.UserJpa;
 import com.nhg.game.domain.room.Room;
 import com.nhg.game.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,5 +12,5 @@ import java.util.List;
 @Repository
 public interface RoomJpaRepository extends JpaRepository<RoomJpa, Integer> {
 
-    List<Room> findByOwner(User owner);
+    List<RoomJpa> findByOwner(UserJpa owner);
 }
