5fb7fd1eee3c3e22e4b918385f211f46bc19efbf
Mattia <82611659+Xhst@users.noreply.github.com>
2024-02-01 17:22:30 +0100

    WebSocketDomainEventListener UpdateRoomEntityEvent

--- game/src/main/java/com/nhg/game/adapter/out/websocket/WebSocketDomainEventListener.java
+++ game/src/main/java/com/nhg/game/adapter/out/websocket/WebSocketDomainEventListener.java
@@ -2,6 +2,9 @@ package com.nhg.game.adapter.out.websocket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhg.common.domain.event.DomainEvent;
+import com.nhg.game.application.event.room.UpdateRoomEntityEvent;
+import com.nhg.game.domain.room.entity.Entity;
+import com.nhg.game.infrastructure.helper.BroadcastHelper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -18,6 +21,16 @@ public class WebSocketDomainEventListener {
     public void handleEvent(DomainEvent domainEvent) {
         try {
             switch (domainEvent) {
+
+                case UpdateRoomEntityEvent event -> {
+                    Entity entity = event.getEntity();
+
+                    BroadcastHelper.sendBroadcastMessage(entity.getRoom().getUsers().values(), new OutgoingPacket(
+                            OutPacketHeaders.UpdateEntity,
+                            objectMapper.writeValueAsString(entity)
+                    ));
+                }
+
                 default -> { }
             }
         } catch (Exception e) {
