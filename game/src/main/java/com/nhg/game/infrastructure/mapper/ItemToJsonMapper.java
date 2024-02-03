a127c6ecb7c8432afc95460a94182145eed28c61
Mattia <82611659+Xhst@users.noreply.github.com>
2024-02-03 09:49:34 +0100

    ItemToJsonMapper

---
+++ game/src/main/java/com/nhg/game/infrastructure/mapper/ItemToJsonMapper.java
@@ -0,0 +1,32 @@
+package com.nhg.game.infrastructure.mapper;
+
+import com.nhg.game.domain.item.Item;
+import lombok.NonNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemToJsonMapper {
+
+    public JSONObject itemToJson(@NonNull Item item) {
+        return new JSONObject()
+                .put("id", item.getId())
+                .put("name", item.getPrototype().getName());
+    }
+
+    public JSONObject itemsToJson(@NonNull Iterable<Item> items) {
+        JSONObject json = new JSONObject();
+        JSONArray data = new JSONArray();
+        try {
+            for (Item item : items) {
+                data.put(itemToJson(item));
+            }
+
+            json.put("data", data);
+        } catch (JSONException ignored) {}
+
+        return json;
+    }
+}
