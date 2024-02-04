5f3bba86cf03122ad49f812b1676c731c6fca4f0
Mattia <82611659+Xhst@users.noreply.github.com>
2024-02-04 09:37:38 +0100

    MultiStateInteraction

---
+++ game/src/main/java/com/nhg/game/domain/item/interaction/MultiStateInteraction.java
@@ -0,0 +1,40 @@
+package com.nhg.game.domain.item.interaction;
+
+import com.nhg.game.application.event.room.UpdateRoomEntityEvent;
+import com.nhg.game.domain.room.entity.Entity;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MultiStateInteraction implements Interaction {
+
+    private Short extraData;
+    private final short stateCount;
+
+    public MultiStateInteraction(String extraData, short stateCount) {
+        this.extraData = Short.parseShort(extraData);
+        this.stateCount = stateCount;
+    }
+
+    @Override
+    public void onClick(Entity entity, Entity interactingEntity) {
+        try {
+            if (stateCount <= 0) return;
+
+            short currentState = extraData;
+
+            if ((currentState + 1) % stateCount == currentState) return;
+
+            extraData = (short) ((currentState + 1) % stateCount);
+
+            entity.getEventPublisher().publish(new UpdateRoomEntityEvent(entity));
+
+        } catch (Exception e) {
+            log.error("Incorrect extraData for item with entityId: "+ entity.getId());
+        }
+    }
+
+    @Override
+    public String getExtraData(Entity entity) {
+        return extraData.toString();
+    }
+}
