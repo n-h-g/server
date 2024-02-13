9f5e0afafb6c858e36fba32ecf4e5bb2d15aab19
Mattia <82611659+Xhst@users.noreply.github.com>
2024-02-13 18:00:22 +0100

    feat: messenger service

---
+++ messenger/src/main/java/com/nhg/messenger/MessengerApplication.java
@@ -0,0 +1,14 @@
+package com.nhg.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerApplication.class, args);
+    }
+}
