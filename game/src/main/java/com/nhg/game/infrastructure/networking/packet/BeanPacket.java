5fa0586c8b27b7cce5f717a94221ad13f3e75b63
Mattia <82611659+Xhst@users.noreply.github.com>
2024-02-16 10:33:41 +0100

    feat: BeanPacket annotation

---
+++ game/src/main/java/com/nhg/game/infrastructure/networking/packet/BeanPacket.java
@@ -0,0 +1,14 @@
+package com.nhg.game.infrastructure.networking.packet;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface BeanPacket {
+    int header();
+}
