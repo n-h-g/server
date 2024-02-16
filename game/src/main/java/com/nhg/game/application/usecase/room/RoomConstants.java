15921285fe741adf50ad35d9bae73b64f999dfbd
Mattia <82611659+Xhst@users.noreply.github.com>
2024-02-16 20:06:58 +0100

    feat: RoomConstants

---
+++ game/src/main/java/com/nhg/game/application/usecase/room/RoomConstants.java
@@ -0,0 +1,6 @@
+package com.nhg.game.application.usecase.room;
+
+public final class RoomConstants {
+    public static final int MAX_ITEMS = 3000;
+    public static final float MAX_HEIGHT = 40f;
+}
