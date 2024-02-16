77de6f7514cb2e26979c6779001af5d9626fc110
Mattia <82611659+Xhst@users.noreply.github.com>
2024-02-16 20:06:46 +0100

    feat: ProblemCode max height

--- game/src/main/java/com/nhg/game/application/exception/ProblemCode.java
+++ game/src/main/java/com/nhg/game/application/exception/ProblemCode.java
@@ -7,7 +7,8 @@ public enum ProblemCode {
     ROOM_DOES_NOT_EXIST("room_does_not_exist"),
     ENTITY_DOES_NOT_EXIST("entity_does_not_exist"),
 
-    MAX_ITEMS("max_items");
+    MAX_ITEMS("max_items"),
+    MAX_HEIGHT("max_height");
 
 
     private final String code;
