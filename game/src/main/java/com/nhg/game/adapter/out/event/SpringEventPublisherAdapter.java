6ecfd551d7c92c99c517a7c1bdcfaf2a36b6c2cc
Mattia <82611659+Xhst@users.noreply.github.com>
2024-02-19 16:39:06 +0100

    feat: publish InfrastructureEvent

--- game/src/main/java/com/nhg/game/adapter/out/event/SpringEventPublisherAdapter.java
+++ game/src/main/java/com/nhg/game/adapter/out/event/SpringEventPublisherAdapter.java
@@ -2,18 +2,25 @@ package com.nhg.game.adapter.out.event;
 
 import com.nhg.common.domain.event.DomainEvent;
 import com.nhg.common.domain.event.DomainEventPublisher;
+import com.nhg.game.infrastructure.event.InfrastructureEvent;
+import com.nhg.game.infrastructure.event.InfrastructureEventPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class SpringEventPublisherAdapter implements DomainEventPublisher {
+public class SpringEventPublisherAdapter implements DomainEventPublisher, InfrastructureEventPublisher {
 
     private final ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     public void publish(DomainEvent event) {
+        applicationEventPublisher.publishEvent(event);
+    }
+
+    @Override
+    public void publish(InfrastructureEvent event) {
         applicationEventPublisher.publishEvent(event);
     }
 }
