f894876d318fcdf416ff5bdeed2cb8e92036fce6
Mattia <82611659+Xhst@users.noreply.github.com>
2024-02-19 17:44:29 +0100

    feat: InfrastructureEventListenerAdapter

---
+++ game/src/main/java/com/nhg/game/adapter/in/event/InfrastructureEventListenerAdapter.java
@@ -0,0 +1,36 @@
+package com.nhg.game.adapter.in.event;
+
+import com.nhg.game.application.usecase.room.entity.LoadRoomEntitiesUseCase;
+import com.nhg.game.infrastructure.event.InfrastructureEvent;
+import com.nhg.game.infrastructure.event.room.RoomTaskStartedEvent;
+import com.nhg.game.infrastructure.event.room.RoomTaskStoppedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class InfrastructureEventListenerAdapter {
+
+    private final LoadRoomEntitiesUseCase loadRoomEntitiesUseCase;
+
+    @EventListener
+    public void handleEvent(InfrastructureEvent infrastructureEvent) {
+        try {
+            switch (infrastructureEvent) {
+                case RoomTaskStartedEvent event -> loadRoomEntitiesUseCase.loadRoomEntities(event.getRoomId());
+
+
+                case RoomTaskStoppedEvent event -> {
+
+                }
+
+                default -> { }
+            }
+        } catch (Exception e) {
+            log.error("InfrastructureEventListenerAdapter: Error while handling event");
+        }
+    }
+}
